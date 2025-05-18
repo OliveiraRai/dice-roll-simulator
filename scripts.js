7b09550c38a2c9475bc503c043dbe63f850237e9
Rai <rai_the_mage@hotmail.com>
2025-05-18 20:12:09 -0300

    added animation to dice

--- scripts.js
+++ scripts.js
@@ -1,8 +1,20 @@
 let arr = [0]; // array with one entity
+let dice = document.getElementById("dice");
 document.getElementById("clear").style.display = "none";
 
 document.getElementById("button").addEventListener("click", () => {
-  const dFaces = ["&#9856;", "&#9857;", "&#9858;", "&#9859;", "&#9860;", "&#9861;"];
+  dice.classList.remove("rotate");
+  void dice.offsetWidth;
+  dice.classList.add("rotate");
+  
+  const dFaces = [
+    "&#9856;",
+    "&#9857;",
+    "&#9858;",
+    "&#9859;",
+    "&#9860;",
+    "&#9861;",
+  ];
 
   let randInt = Math.floor(Math.random() * 6); // número aleatório de 0 a 5
   let randDice = dFaces[randInt];
@@ -20,7 +32,7 @@ document.getElementById("button").addEventListener("click", () => {
 });
 
 document.getElementById("clear").addEventListener("click", () => {
-  document.getElementById("roll-history").innerHTML = '';
+  document.getElementById("roll-history").innerHTML = "";
   arr.length = 1; // reseta mantendo o 0
   document.getElementById("clear").style.display = "none"; // esconde o botão imediatamente
 });

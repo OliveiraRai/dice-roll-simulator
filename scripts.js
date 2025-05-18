cc45e148bc4c87a51d31900d9a76a83e045a1e77
Rai <rai_the_mage@hotmail.com>
2025-05-18 19:19:15 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,26 @@
+let arr = [0]; // array with one entity
+document.getElementById("clear").style.display = "none";
+
+document.getElementById("button").addEventListener("click", () => {
+  const dFaces = ["&#9856;", "&#9857;", "&#9858;", "&#9859;", "&#9860;", "&#9861;"];
+
+  let randInt = Math.floor(Math.random() * 6); // número aleatório de 0 a 5
+  let randDice = dFaces[randInt];
+
+  document.getElementById("dice").innerHTML = randDice;
+
+  let successor = arr[arr.length - 1] + 1;
+  arr.push(successor); // Primeiro adiciona ao array
+
+  document.getElementById("clear").style.display = "inline";
+
+  document.getElementById("roll-history").innerHTML += `
+    <li><span>Roll ${arr[arr.length - 1]}</span><span>${randDice}</span></li>
+  `;
+});
+
+document.getElementById("clear").addEventListener("click", () => {
+  document.getElementById("roll-history").innerHTML = '';
+  arr.length = 1; // reseta mantendo o 0
+  document.getElementById("clear").style.display = "none"; // esconde o botão imediatamente
+});
